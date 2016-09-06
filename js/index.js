69527c1a3ccf99658c299965d36e4ae6bc114005
Mariana <m.nyshpor@gmail.com>
2016-09-06 09:11:09 +0000

    newmobile

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+
+$(function(){
+  $('#btn-send').click(sendForm);  
+});
+function sendForm (e){
+    e.preventDefault();
+    $.ajax({
+    url: "https://formspree.io/m.nyshpor@gmail.com", 
+    method: "POST",
+    data: {
+        message:$('#msg').val()
+    },
+    dataType: "json",
+    success:function(){
+        $('#thanks').html('Thank you for contacting us.');
+    }
+});
+};
